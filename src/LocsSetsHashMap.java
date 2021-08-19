65a71403c812e2d4f08eefda7039fd3c668285ca
radek-czcz <radowiczek@gmail.com>
2021-08-19 19:01:53 +0200

    fix src/LocsSetsHashMap.java. Wrong logic wanted to operate on already deleted locsSet

--- src/LocsSetsHashMap.java
+++ src/LocsSetsHashMap.java
@@ -9,32 +9,19 @@ public class LocsSetsHashMap<K, V> extends HashMap<K, V> {
 	public void registerListener(ILocsSetsListener listener) {
 		listenersList.add(listener);
 	}
-	
-	@Override
-	public void clear() {
-		// TODO Auto-generated method stub
-		super.clear();
-	}
-	
-	@Override
-	public V put(K key, V value) {
-		// TODO Auto-generated method stub
-		return super.put(key, value);
-	}
-	
-	@Override
-	public void putAll(Map<? extends K, ? extends V> m) {
-		// TODO Auto-generated method stub
-		super.putAll(m);
-	}
-	
 	@Override
 	public V remove(Object key) {
 		// TODO Auto-generated method stub
 		V temp;
+		for (ILocsSetsListener locsSetsListener : listenersList) {
+			if (locsSetsListener instanceof respModel) {}
+			else locsSetsListener.locsSetsChanged(key);
+		}
 		temp = super.remove(key);
 		for (ILocsSetsListener locsSetsListener : listenersList) {
-			locsSetsListener.locsSetsChanged();;
+			if (locsSetsListener instanceof respModel) {
+			locsSetsListener.locsSetsChanged(key);
+			} else {}
 		}
 		return temp;
 	}
