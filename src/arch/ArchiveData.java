718ff97b379d7455cdd6679ccd6f6b19338c346e
radek-czcz <radowiczek@gmail.com>
2021-08-24 11:20:00 +0200

    ArchiveData to package arch moved -> moved to directory arch (renamed)

--- src/arch/ArchiveData.java
+++ src/arch/ArchiveData.java
@@ -1,10 +1,14 @@
+package arch;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.ItemSelectable;
 import java.awt.LayoutManager;
 import java.awt.Window;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
 import java.awt.event.KeyListener;
@@ -15,47 +19,50 @@ import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
+import javax.swing.AbstractButton;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.RootPaneContainer;
+import javax.swing.text.JTextComponent;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import entry.PathProvider;
+import package1.locsSets;
 
 public class ArchiveData {
 	
-	static private SortedSet<File> setOfArchives; // = new TreeSet<>();
+	static private SortedSet<ArchiveData> setOfArchives;
 
-	public SortedSet<File> getSetOfArchives() {
+	public static SortedSet<ArchiveData> getSetOfArchives() {
 		return setOfArchives;
 	}
-
-	void setSetOfArchives(SortedSet<File> setOfArchives) {
-		this.setOfArchives = setOfArchives;
+	
+	public static void addToSetOfArchives() {
+		File locAndName = LocAndNameWindow.getPath();
 	}
 	
-	public void addToSetOfArchives() {
-		File locAndName = getLocationOfFilesFromUser();
-		//setOfArchives.add(directory);
-	}
-
-	private File getLocationOfFilesFromUser() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	String name;
+	locsSets data;
 	
-	public static void main(String[] args) {
-	
+	ArchiveData(String name) {
+		this.name = name;
+		data = new locsSets(this);
 	}
 
 static class LocAndNameWindow {
+	
+	private static String name;
+	private static File path;
+	
 	public static void main(String[] args) {
 		first();
 	}
@@ -67,11 +74,13 @@ static class LocAndNameWindow {
 		//setOfArchives = (SortedSet<File>)aC.getBean("tree");
 		
 		Component dialog = (Component)aC.getBean("entryWindow");
-																	System.out.println(aC.getType("entryWindow"));
 		Container cntr = (Container)dialog;
 		RootPaneContainer rpc = (RootPaneContainer)cntr;
 		JComponent jL;
-		
+		{
+		Container cntr2 = (Container)cntr.getComponent(0);
+		System.out.println("end");
+		}
 		rpc.getContentPane().setLayout((LayoutManager)aC.getBean("layout"));
 		
 		// maybe not needed - listeners
@@ -114,7 +123,7 @@ static class LocAndNameWindow {
 		c0.setBackground(new Color(200,150,90));
 
 		//JtextField name's input field
-		Component c1 = rpc.getContentPane().add(new JTextField("abc"));
+		Component c1 = rpc.getContentPane().add((Component)aC.getBean("TextFieldBean"));
 		c1.setPreferredSize(new Dimension(100, 20));
 		
 		//JLabel "drive selection"
@@ -129,11 +138,40 @@ static class LocAndNameWindow {
 		Component c3 = rpc.getContentPane().add(new JComboBox(drives));
 		c3.setPreferredSize(new Dimension(100, 20));
 		
+		//Button "read Data"
+		Component jB =  rpc.getContentPane().add((Component)aC.getBean("ButtonBean"));
+		AbstractButton aB = (AbstractButton)jB;
+		aB.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				// name object
+				JTextComponent stringComp = (JTextComponent)c1;
+				
+				//build archive File
+				ItemSelectable iS = (ItemSelectable)c3;
+				Object[] archive = iS.getSelectedObjects();
+				path = new File(archive[0].toString());
+				
+				System.out.println( "perform action: name of archive=" 
+				+ stringComp.getText() 
+				+ " selected=" + path);
+			}
+		});
+		
 		dialog.setSize(200, 150);
 		dialog.setVisible(true);
 		
 		Window window = (Window)dialog;
 		window.addWindowListener((WindowListener) aC.getBean("WindowListenerBean"));
 		}
+
+	static String getName() {
+		return name;
+	}
+
+	static File getPath() {
+		return path;
+	}
 	}
 }
